dce89ec0c35c4343c03f490ea9aa36c900a7bfa3
Oh-Myeongjae <omjcws@gmail.com>
2022-07-21 20:01:42 +0900

    Create busMain.java
    버스시나리오 확인용 class 입니다.

---
+++ busMain.java
@@ -0,0 +1,51 @@
+package test;
+
+public class busMain {
+	public static void main(String[] args) {		
+		Bus aBus = new Bus();
+		Bus bBus = new Bus();
+		System.out.println("1번시나리오");
+		aBus.busInfo();
+		bBus.busInfo();
+	
+		System.out.println();
+		System.out.println("2번시나리오");
+		aBus.Board(2);
+		aBus.busInfo();
+		
+		System.out.println();
+		System.out.println("3번시나리오");
+		aBus.changefuel(-50);
+		aBus.busInfo();
+		
+		System.out.println();
+		System.out.println("4번시나리오");
+		aBus.changeState("차고지행");
+		aBus.busInfo();
+
+		System.out.println();
+		System.out.println("5번시나리오");
+		aBus.changefuel(10);
+		aBus.busInfo();
+		
+		System.out.println();
+		System.out.println("6번시나리오");
+		aBus.changeState("운행중");
+		aBus.busInfo();
+
+		System.out.println();
+		System.out.println("7번시나리오");
+		aBus.Board(45);
+
+		System.out.println();
+		System.out.println("8번시나리오");
+		aBus.Board(5);
+		aBus.busInfo();
+		
+		System.out.println();
+		System.out.println("9번시나리오");
+		aBus.changefuel(-55);
+		aBus.busInfo();
+	}
+
+}
